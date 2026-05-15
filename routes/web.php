224c90f75c29ae00d22662450f78df466f17c8a5
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-05-15 18:00:46 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ChatController;
+
+Route::get('/', [ChatController::class, 'index']);
+Route::post('/chat', [ChatController::class, 'chat']);
